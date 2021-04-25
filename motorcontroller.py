d69e7bf52e7311047c32268c0d21dda9ecdfcf61
Dimitar Bezhanovski <dimitarbez@gmail.com>
2021-04-25 23:45:49 +0200

    refactored motorcontroller

--- motorcontroller.py
+++ motorcontroller.py
@@ -1,17 +1,24 @@
 import RPi.GPIO as GPIO
 
+motor1_pwm_pin = 8
+motor2_pwm_pin = 10
+motor_in1 = 5
+motor_in2 = 11
+motor_in3 = 13
+motor_in4 = 15
+
 class MotorController:
 
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(8, GPIO.OUT)
-        GPIO.setup(10, GPIO.OUT)
-        GPIO.setup(5, GPIO.OUT)
-        GPIO.setup(11, GPIO.OUT)
-        GPIO.setup(13, GPIO.OUT)
-        GPIO.setup(15, GPIO.OUT)
-        self.pwm1 = GPIO.PWM(8, 200)
-        self.pwm2 = GPIO.PWM(10, 200)
+        GPIO.setup(motor1_pwm_pin, GPIO.OUT)
+        GPIO.setup(motor2_pwm_pin, GPIO.OUT)
+        GPIO.setup(motor_in1, GPIO.OUT)
+        GPIO.setup(motor_in2, GPIO.OUT)
+        GPIO.setup(motor_in3, GPIO.OUT)
+        GPIO.setup(motor_in4, GPIO.OUT)
+        self.pwm1 = GPIO.PWM(motor1_pwm_pin, 200)
+        self.pwm2 = GPIO.PWM(motor2_pwm_pin, 200)
         self.motorspeed = 50
         self.pwm1.start(self.motorspeed)
         self.pwm2.start(self.motorspeed)
@@ -33,43 +40,43 @@ class MotorController:
         self.pwm2.ChangeDutyCycle(self.motorspeed)
 
     def moveforward(self):
-        GPIO.output(5, GPIO.LOW)
-        GPIO.output(11, GPIO.HIGH)
-        GPIO.output(13, GPIO.HIGH)
-        GPIO.output(15, GPIO.LOW)
+        GPIO.output(motor_in1, GPIO.LOW)
+        GPIO.output(motor_in2, GPIO.HIGH)
+        GPIO.output(motor_in3, GPIO.HIGH)
+        GPIO.output(motor_in4, GPIO.LOW)
 
     def movebackward(self):
-        GPIO.output(5, GPIO.HIGH)
-        GPIO.output(11, GPIO.LOW)
-        GPIO.output(13, GPIO.LOW)
-        GPIO.output(15, GPIO.HIGH)
+        GPIO.output(motor_in1, GPIO.HIGH)
+        GPIO.output(motor_in2, GPIO.LOW)
+        GPIO.output(motor_in3, GPIO.LOW)
+        GPIO.output(motor_in4, GPIO.HIGH)
 
     def moveleft(self):
-        GPIO.output(5, GPIO.LOW)
-        GPIO.output(11, GPIO.HIGH)
-        GPIO.output(13, GPIO.LOW)
-        GPIO.output(15, GPIO.LOW)
+        GPIO.output(motor_in1, GPIO.LOW)
+        GPIO.output(motor_in2, GPIO.HIGH)
+        GPIO.output(motor_in3, GPIO.LOW)
+        GPIO.output(motor_in4, GPIO.LOW)
 
     def moveright(self):
-        GPIO.output(5, GPIO.LOW)
-        GPIO.output(11, GPIO.LOW)
-        GPIO.output(13, GPIO.HIGH)
-        GPIO.output(15, GPIO.LOW)
+        GPIO.output(motor_in1, GPIO.LOW)
+        GPIO.output(motor_in2, GPIO.LOW)
+        GPIO.output(motor_in3, GPIO.HIGH)
+        GPIO.output(motor_in4, GPIO.LOW)
 
     def movehardright(self):
-        GPIO.output(5, GPIO.HIGH)
-        GPIO.output(11, GPIO.LOW)
-        GPIO.output(13, GPIO.HIGH)
-        GPIO.output(15, GPIO.LOW)
+        GPIO.output(motor_in1, GPIO.HIGH)
+        GPIO.output(motor_in2, GPIO.LOW)
+        GPIO.output(motor_in3, GPIO.HIGH)
+        GPIO.output(motor_in4, GPIO.LOW)
 
     def movehardleft(self):
-        GPIO.output(5, GPIO.LOW)
-        GPIO.output(11, GPIO.HIGH)
-        GPIO.output(13, GPIO.LOW)
-        GPIO.output(15, GPIO.HIGH)
+        GPIO.output(motor_in1, GPIO.LOW)
+        GPIO.output(motor_in2, GPIO.HIGH)
+        GPIO.output(motor_in3, GPIO.LOW)
+        GPIO.output(motor_in4, GPIO.HIGH)
 
     def stop(self):
-        GPIO.output(5, GPIO.LOW)
-        GPIO.output(11, GPIO.LOW)
-        GPIO.output(13, GPIO.LOW)
-        GPIO.output(15, GPIO.LOW)
+        GPIO.output(motor_in1, GPIO.LOW)
+        GPIO.output(motor_in2, GPIO.LOW)
+        GPIO.output(motor_in3, GPIO.LOW)
+        GPIO.output(motor_in4, GPIO.LOW)
